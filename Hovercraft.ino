b1d48decc2b91a11aa97b95f09509f4617d0f699
rima1881 <amd.gharibipour@outlook.com>
2025-04-06 17:22:06 -0400

    added imu

--- Hovercraft.ino
+++ Hovercraft.ino
@@ -7,15 +7,16 @@ int main(){
 
   init_timer_0();
   init_timer_1();
+  init_timer_2();
+  init_imu();
 
-
-  init_lift();
-  set_lift(true);
   set_servo_A(90);
   set_servo_B(-90);
+  
+  init_lift();
+  set_lift(true);
 
-
-  set_fan_A(255);
+  set_fan_A(255); // set both fans full power
   set_fan_B(255);
 
 
